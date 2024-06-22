12bbf4933047d8f85fb886d8aee5b9fa8f9be852
unitiaathira <athirashibuathi13@gmail.com>
2024-06-22 19:39:54 +0530

    added countries migration fields

--- database/migrations/2024_06_22_121711_create_countries_table.php
+++ database/migrations/2024_06_22_121711_create_countries_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('countries', function (Blueprint $table) {
             $table->id();
+            $table->string('code', 3)->primary();
+            $table->string('name', 255);
+            $table->jsonb('states')->nullable();
             $table->timestamps();
         });
     }
