377e97e4620d8442ab475ebd20856fbd81e29e3a
unitiaathira <athirashibuathi13@gmail.com>
2024-06-22 19:44:13 +0530

    added cart item migration fields

--- database/migrations/2024_06_22_121724_create_cart_items_table.php
+++ database/migrations/2024_06_22_121724_create_cart_items_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\User;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,6 +14,9 @@ return new class extends Migration
     {
         Schema::create('cart_items', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class, 'user_id');
+            $table->foreignId('product_id')->references('id')->on('products');
+            $table->integer('quantity');
             $table->timestamps();
         });
     }
