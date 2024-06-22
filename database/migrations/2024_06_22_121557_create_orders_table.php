3745afdb1ceeeafa754919f44ebe3cf64be42372
unitiaathira <athirashibuathi13@gmail.com>
2024-06-22 19:31:08 +0530

    added order table

--- database/migrations/2024_06_22_121557_create_orders_table.php
+++ database/migrations/2024_06_22_121557_create_orders_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\User;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,7 +14,11 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->decimal('total_price', 20, 2);
+            $table->string('status', 45);
             $table->timestamps();
+            $table->foreignIdFor(User::class, 'created_by')->nullable();
+            $table->foreignIdFor(User::class, 'updated_by')->nullable();
         });
     }
 
