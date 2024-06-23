d6711deb057389bb54f5e1a4aefa06629a306119
unitiaathira <athirashibuathi13@gmail.com>
2024-06-23 12:06:56 +0530

    removed key issue and duplicate field issue

--- database/migrations/2024_06_22_121711_create_countries_table.php
+++ database/migrations/2024_06_22_121711_create_countries_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('countries', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id');
             $table->string('code', 3)->primary();
             $table->string('name', 255);
             $table->jsonb('states')->nullable();
--- database/migrations/2024_06_22_121901_create_customer_addresses_table.php
+++ database/migrations/2024_06_22_121901_create_customer_addresses_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('city');
             $table->string('statte', 45)->nullable();
             $table->string('zipcode', 45);
-            $table->string('country_code', 3);
+            // $table->string('country_code', 3);
             $table->foreignId('customer_id')->references('id')->on('customers');
             $table->foreignId('country_code')->references('code')->on('countries');
             $table->timestamps();
