8b7072b6fde1751c68990d839dc1a27033c285e4
unitiaathira <athirashibuathi13@gmail.com>
2024-06-23 11:48:46 +0530

    added customer address migration fields

--- database/migrations/2024_06_22_121901_create_customer_addresses_table.php
+++ database/migrations/2024_06_22_121901_create_customer_addresses_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('customer_addresses', function (Blueprint $table) {
             $table->id();
+            $table->string('type', 45);
+            $table->string('address1', 255);
+            $table->string('address2', 255);
+            $table->string('city');
+            $table->string('statte', 45)->nullable();
+            $table->string('zipcode', 45);
+            $table->string('country_code', 3);
+            $table->foreignId('customer_id')->references('id')->on('customers');
+            $table->foreignId('country_code')->references('code')->on('countries');
             $table->timestamps();
         });
     }
